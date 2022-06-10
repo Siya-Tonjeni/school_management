b7eb577908d0509428d9108d91d63b63e6db5483
Siyamtanda Tonjeni <217107958@mycput.ac.za>
2022-06-10 17:32:09 +0200

    Address Domain

---
+++ src/main/java/za/ac/cput/school_management/domain/Address.java
@@ -0,0 +1,97 @@
+/*
+ * Address.java
+ * Domain class for Address class
+ * Author: Siyamtanda (217107958)
+ * Date: 9 June 2022
+ */
+package za.ac.cput.school_management.domain;
+
+public class Address {
+
+    private String unitNumber;
+    private String complexName;
+    private String streetNumber;
+    private String streetName;
+    private String postalCode;
+    private City city;
+
+    public String getUnitNumber() {
+        return unitNumber;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "unitNumber='" + unitNumber + '\'' +
+                ", complexName='" + complexName + '\'' +
+                ", streetNumber='" + streetNumber + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city=" + city +
+                '}';
+    }
+
+    private Address (Builder builder){
+        this.unitNumber = builder.unitNumber;
+        this.complexName = builder.complexName;
+        this.streetNumber = builder.streetNumber;
+        this.streetName = builder.streetName;
+        this.postalCode = builder.postalCode;
+        this.city = builder.city;
+    }
+
+
+    public static class Builder{
+        private String unitNumber;
+        private String complexName;
+        private String streetNumber;
+        private String streetName;
+        private String postalCode;
+        private City city;
+
+        public Builder setUnitNumber(String unitNumber) {
+            this.unitNumber = unitNumber;
+            return this;
+        }
+
+        public Builder setComplexName(String complexName) {
+            this.complexName = complexName;
+            return this;
+        }
+
+        public Builder setStreetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+            return this;
+        }
+
+        public Builder setStreetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+
+        public Builder setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+
+        public Builder setCity(City city) {
+            this.city = city;
+            return this;
+        }
+
+        private Builder copy (Address address){
+            this.unitNumber = address.unitNumber;
+            this.complexName = address.complexName;
+            this.streetNumber = address.streetNumber;
+            this.streetName = address.streetName;
+            this.postalCode = address.postalCode;
+            this.city = address.city;
+
+            return this;
+        }
+        public Address builder() { return new Address(this); }
+
+    }
+
+}
