34392828a89c92b2c3ac7508a5f288ee679d96fa
ZintleZothe <216130565@mycput.ac.za>
2022-06-10 22:58:11 +0200

    modified City class and added StudentAddress class

---
+++ src/main/java/za/ac/cput/school_management/factory/StudentAddressFactory.java
@@ -0,0 +1,10 @@
+/**
+ * StudentFactory.java
+ * Factory class for StudentAddress
+ * Author:Zintle Zothe(216130565)
+ * Date:  10 June 2022
+ */
+package za.ac.cput.school_management.factory;
+
+public class StudentAddressFactory {
+}
