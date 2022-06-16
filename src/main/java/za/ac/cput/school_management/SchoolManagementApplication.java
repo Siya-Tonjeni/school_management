6c3c76fcf71360eb3d3c9d670f471e3401cc388a
Sonwabile <219267189@mycput.ac.za>
2022-06-16 16:37:20 +0100

    Added Springboot Dependencies in pom.xml and SchoolManagementApplication class.

---
+++ src/main/java/za/ac/cput/school_management/SchoolManagementApplication.java
@@ -0,0 +1,17 @@
+/**
+ * SchoolManagementApplication.java
+ * Main class to run SpringBoot
+ * Author: Sonwabile Gxoyiya (219267189)
+ * Date: 16 June 2022
+ */
+package za.ac.cput.school_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementApplication.class, args);
+    }
+}
