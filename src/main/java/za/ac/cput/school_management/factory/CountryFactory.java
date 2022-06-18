5321e8497f8aa6ac0d65758f76725fcf13757324
Lee <214254151>
2022-06-18 02:41:38 +0200

    Created CountryServices with its interfaces

--- src/main/java/za/ac/cput/school_management/factory/CountryFactory.java
+++ src/main/java/za/ac/cput/school_management/factory/CountryFactory.java
@@ -5,14 +5,16 @@ package za.ac.cput.school_management.factory;
  * Author: Abongile Tshopi (214254151)
  * Date Created: 14 June 2022
  */
-
 import za.ac.cput.school_management.domain.Country;
 import za.ac.cput.school_management.util.helper;
 
 public class CountryFactory {
     public static Country build( String id, String name){
 
-//        String id = helper.generateUUID();
+//        helper.checkStringParam("id", id);
+//        helper.checkStringParam("name", name);
+
+        //String id = helper.generateUUID();
 
         return new Country.Builder()
                 .setId(id)
