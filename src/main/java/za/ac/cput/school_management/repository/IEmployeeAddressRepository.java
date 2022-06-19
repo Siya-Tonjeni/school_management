46dc6eec4cda5959a353bd34d0de288e4a642fff
nmkontshwana <214236714@mycput.ac.za>
2022-06-19 03:09:51 +0200

    EmployeeAddressFactoryTest,RepositoryTest,ServiceTest

--- src/main/java/za/ac/cput/school_management/repository/IEmployeeAddressRepository.java
+++ src/main/java/za/ac/cput/school_management/repository/IEmployeeAddressRepository.java
@@ -1,14 +1,13 @@
 
-
+/*
+   EmployeeAddress.java
+   Interface for EmployeeAddress
+   Student: Nikiwe Mkontshwana
+   Student Number: 214236714
+   Date: 14 June 2022
+*/
 package za.ac.cput.school_management.repository;
 
- /*
-    EmployeeAddress.java
-    Interface for EmployeeAddress
-    Student: Nikiwe Mkontshwana
-    Student Number: 214236714
-    Date: 14 June 2022
- */
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.school_management.domain.EmployeeAddress;
